aee95e44a9d42d2abd53369eb35d2e2ac144a78d
Silvia Chung <schung97@gmail.com>
2018-01-16 17:11:57 -0500

    Done.

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,18 @@
-// Code ThemedDecoration Component Here
+import React from 'react';
+
+export default class ThemedDecoration extends React.Component {
+
+  render() {
+    console.log("props:", this.props.children)
+    console.log("props 2 layer deep, text:", this.props.children[0].props.children)
+    // console.log("props 2 layer deep", this.props.children[0].)
+    console.log("Children", React.Children)
+
+    const passdownprop = React.Children.map(this.props.children, child => React.cloneElement(child, { className: this.props.theme}))
+    return(
+      <div>
+      {passdownprop}
+      </div>
+    )
+  }
+}
